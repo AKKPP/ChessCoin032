db1e3b491f93c1ff4a8e21029d1b7008e6bdf343
Laser9un <laser9un@gmail.com>
2020-09-11 15:52:25 +0300

    Generate chesscoin.conf automatically

--- src/util.cpp
+++ src/util.cpp
@@ -1046,6 +1046,35 @@ const boost::filesystem::path &GetDataDir(bool fNetSpecific)
     return path;
 }
 
+string randomStrGen(int length) {
+    static string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    string result;
+    result.resize(length);
+    for (int32_t i = 0; i < length; i++)
+        result[i] = charset[rand() % charset.length()];
+
+    return result;
+}
+
+void createConf()
+{
+    srand(time(NULL));
+
+    ofstream pConf;
+    pConf.open(GetConfigFile().generic_string().c_str());
+    pConf << "rpcuser=user\nrpcpassword="
+            + randomStrGen(15)
+            + "\nrpcport=7324"
+            + "\nport=7323"
+            + "\ndaemon=0 #(0=off, 1=on) Run in the background as a daemon and accept commands"
+            + "\nserver=0 #(0=off, 1=on) Accept command line and JSON-RPC commands"
+            + "\nrpcallowip=127.0.0.1"
+            + "\naddnode=66.70.191.185:7323"
+            + "\naddnode=51.79.145.189:7323"
+            + "\naddnode=95.222.251.44:7323";						
+    pConf.close();
+}
+
 boost::filesystem::path GetConfigFile()
 {
     boost::filesystem::path pathConfigFile(GetArg("-conf", "chesscoin.conf"));
@@ -1058,8 +1087,12 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
 {
     boost::filesystem::ifstream streamConfig(GetConfigFile());
     if (!streamConfig.good())
-        return; // No bitcoin.conf file is OK
-
+    {
+        createConf();
+        new(&streamConfig) boost::filesystem::ifstream(GetConfigFile());
+        if(!streamConfig.good())
+            return;
+    }
     set<string> setOptions;
     setOptions.insert("*");
 
