021aa0755df27a6a0b5575fe1538b30e1c073d06
OleksandrAAA <aleksandkolesnik73@gmail.com>
2023-01-23 23:24:52 +0700

    v1.4.4: Updated document for daemon build.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![ChessCoin](https://i.ibb.co/vLmj7dY/256x256.png)](https://bitcointalk.org/index.php?topic=1510517.new;topicseen#new)
+[![ChessCoin](https://i.ibb.co/nRwWs6v/256x256.png)](https://bitcointalk.org/index.php?topic=1510517.new;topicseen#new)
 
 
 ChessCoin 0.32% development tree
--- src/clientversion.h
+++ src/clientversion.h
@@ -20,7 +20,7 @@
 // These need to be macros, as version.cpp's and bitcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       1
 #define CLIENT_VERSION_MINOR       4
-#define CLIENT_VERSION_REVISION    3
+#define CLIENT_VERSION_REVISION    4
 
 // Converts the parameter X to a string after macro replacement on X has been performed.
 // Don't merge these into one macro!
